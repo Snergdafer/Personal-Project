bf518446ca44d2de7c40d57cfe0c952f699f6e8f
Jesse Etherington <jesse.etherington@gmail.com>
2021-03-12 17:38:31 -0700

    more stuff

--- phanstasy_costco/src/components/Menu.js
+++ phanstasy_costco/src/components/Menu.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react'
 import axios from 'axios'
 import {withRouter, Link} from 'react-router-dom'
 import {logoutUser} from '../redux/userReducer'
-import UpdateStore from '../hooks/UpdateStore.js'
+import UpdateStore from './UpdateStore.js'
 import {updateSearch} from '../redux/itemsReducer'
 import {connect} from 'react-redux'
 
